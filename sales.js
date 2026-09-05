0c1157516982fcec78d48f682c9e282e8340c8ff
armasantos <armasantos@users.noreply.github.com>
2026-09-05 13:44:59 -0300

    feat: reconstruir landing page premium mvp 4

--- sales.js
+++
@@ -1,9 +0,0 @@
-const query = new URLSearchParams(window.location.search);
-const utm = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content'].reduce((values, key) => { if (query.get(key)) values[key] = query.get(key); return values; }, {});
-if (Object.keys(utm).length) sessionStorage.setItem('clubeCardapioUtm', JSON.stringify(utm));
-document.querySelectorAll('[data-checkout]').forEach((link) => link.addEventListener('click', () => {
-  const destination = new URL(link.href, window.location.href);
-  const savedUtm = JSON.parse(sessionStorage.getItem('clubeCardapioUtm') || '{}');
-  Object.entries(savedUtm).forEach(([key, value]) => destination.searchParams.set(key, value));
-  link.href = destination.toString();
-}));
